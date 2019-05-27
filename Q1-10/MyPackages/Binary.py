0dce6bb54976c64ad8bac7de54fe57ce0f0decb9
norara13 <s16161@tokyo.kosen-ac.jp>
2019-05-27 21:55:43 +0900

    Q3(2値化)完成

---
+++ Q1-10/MyPackages/Binary.py
@@ -0,0 +1,11 @@
+# 与えられたグレースケール化された画像を二値画像に変換するプログラム
+import numpy as np
+
+def Binary(gray) :
+    # result = [0 if i < 128 else 1 for i in gray]
+    # なんで通らないんだ？？
+    t = 128
+    pic = gray.copy()
+    pic[gray < t] = 0
+    pic[gray > t] = 255
+    return pic
--- Q1-10/Q3.py
+++ Q1-10/Q3.py
@@ -1,4 +1,12 @@
-# 二値化
-from Q2 import rgb_to_gray
+# グレースケール化してから二値化
+from MyPackages.Binary import Binary
+from MyPackages.rgb_to_gray import rgb_to_gray
 import cv2
 
+img = cv2.imread("source/imori.jpg")
+
+gray = rgb_to_gray(img)
+
+bi = Binary(gray)
+
+cv2.imwrite("answer/answer_Q3.jpg", bi)
